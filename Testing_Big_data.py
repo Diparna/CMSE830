41ce83a76624f51b46a1aeabb42aae3771aadf3b
Diparna <36377191+Diparna@users.noreply.github.com>
2024-10-19 17:54:59 -0400

    Add files via upload
    Added descriptions.

--- Testing_Big_data.py
+++ Testing_Big_data.py
@@ -20,14 +20,14 @@ import time
 # Load the data
 @st.cache_data
 def load_data():
-    url = "https://raw.githubusercontent.com/Diparna/CMSE830/refs/heads/main/appended_plane_crash_data.csv?token=GHSAT0AAAAAACY56DOQKCQZBOO7UTLVYC3SZYQPCJQ"
+    url = "https://raw.githubusercontent.com/Diparna/CMSE830/refs/heads/main/appended_plane_crash_data.csv"
     df = pd.read_csv(url)
     df['year'] = pd.to_numeric(df['year'], errors='coerce')
     #df = df.dropna(subset=['year'])
     df['year'] = df['year'].astype(int)
-    #df['date'] = pd.to_datetime(df['date'], errors='coerce')
+    df['date'] = pd.to_datetime(df['date'], errors='coerce')
     #df['date'] = pd.to_datetime(df['date'], format='%m/%d/%Y')
-    df['date'] = pd.to_datetime(df['month'].astype(str) + ' ' + df['year'].astype(str), format='%B %Y', errors='coerce')
+    #df['date'] = pd.to_datetime(df['month'].astype(str) + ' ' + df['year'].astype(str), format='%B %Y', errors='coerce')
     
     numeric_columns = ['aboard', 'fatalities']
     for col in numeric_columns:
@@ -42,7 +42,7 @@ st.title("Plane Crash Data Analysis")
 
 # Sidebar
 st.sidebar.header("Navigation")
-page = st.sidebar.selectbox("Choose a page", ["Overview", "Missing Data Analysis", "Initial Data Analysis", "Exploratory Data Analysis", "Temporal Analysis", "Operator Analysis", "Aircraft Analysis", "Aircraft Survival Analysis" ,"Location Analysis", "Animation Analysis"])
+page = st.sidebar.selectbox("Choose a page", ["Overview", "Missing Data Analysis", "Initial Data Analysis", "Exploratory Data Analysis", "Temporal Analysis", "Operator Analysis", "Aircraft Analysis", "Aircraft Survival Analysis" ,"Location Analysis", "Animation Analysis", "Summary"])
 
 
 # Year range filter
@@ -128,7 +128,7 @@ elif page == "Missing Data Analysis":
     
     # Imputation options
     st.header("Data Imputation")
-    st.markdown("This is a for trying various imputation methods. The error pops up when there's nothing selected in the columns section.")
+    st.markdown("This is a for trying various imputation methods. I have used only simple imputer(I felt like there's no need to go for advanced imputation for my project.).The error pops up when there's nothing selected in the columns section. All the imputation do not work for all the columns. The Most Frequent work on both numeric and text based data.")
     imputation_method = st.selectbox("Select imputation method", ["Mean", "Median", "Most Frequent", "Constant"])
     columns_to_impute = st.multiselect("Select columns to impute", filtered_df.columns)
     
@@ -148,7 +148,15 @@ elif page == "Missing Data Analysis":
         
     st.write("Data after imputation:")
     st.write(filtered_df_imputed[columns_to_impute].head())
-        
+
+    # Display the existing data
+
+    st.header("Missing Data Overview Before Imputation")
+    missing_data = filtered_df.isnull().sum().sort_values(ascending=False)
+    missing_percent = 100 * filtered_df.isnull().sum() / len(filtered_df)
+    missing_table = pd.concat([missing_data, missing_percent], axis=1, keys=['Missing Values', 'Percentage'])
+    st.write(missing_table)
+
     # Update missing data information after imputation
     st.header("Missing Data Overview After Imputation")
     missing_data_after = filtered_df_imputed.isnull().sum().sort_values(ascending=False)
@@ -163,6 +171,7 @@ elif page == "Initial Data Analysis":
     
     with tab1:
         st.header("Dataset Overview")
+        st.markdown(" This tab includes the generic info about the Dataset. ")
         st.write(f"Number of rows: {filtered_df.shape[0]}")
         st.write(f"Number of columns: {filtered_df.shape[1]}")
         st.write("Column names and data types:")
@@ -181,6 +190,7 @@ elif page == "Initial Data Analysis":
     
     with tab4:
         st.header("Data Distribution")
+        st.markdown("As the name suggests, it shows the count of the selected columns. I feel that the graph For aboard and fatalities is difficult to read. So, let's say, it is at 200 and the count is at 10. So that means there are 10 incidents where the number of people abord the flight and the number of people who died is 200.")
         # Select only numeric columns for bar chart
         numeric_cols = filtered_df.select_dtypes(include=['int64', 'float64']).columns
 
@@ -218,7 +228,7 @@ elif page == "Initial Data Analysis":
 
 elif page == "Exploratory Data Analysis":
     st.title("Exploratory Data Analysis")
-    st.markdown("This page shows various things that were done for Exploratory Data Analysis(EDA) of the dataset.")
+    st.markdown("This page shows various things that were done for Exploratory Data Analysis(EDA) of the dataset. I did an analysis on most of the categories I could think of such as Operators, Aircrafts, Common Locations and Number of people who died in the crash.")
     tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Temporal Trends", "Fatality Analysis", "Operator and Aircraft Analysis", "Geographical Analysis", "Correlation Analysis", "Word Map"])
     
     with tab1:
@@ -246,9 +256,13 @@ elif page == "Exploratory Data Analysis":
     
     with tab5:
         st.header("Correlation Analysis")
-        corr_matrix = filtered_df[['aboard', 'fatalities', 'year']].corr()
-        fig = px.imshow(corr_matrix, text_auto=True, aspect="auto")
-        st.plotly_chart(fig)
+        st.markdown("For the correlation matrix, I tried including the other columns such as operators, countries and location but it said that it cannot convert these columns into float. So, I was not sure how to show correlation between them.")
+        #corr_matrix = filtered_df[['aboard', 'fatalities', '']].corr()
+        #fig = px.imshow(corr_matrix, text_auto=True, aspect="auto")
+        #st.plotly_chart(fig)
+        corr_matrix_1 = filtered_df[['aboard', 'fatalities', 'year']].corr()
+        fig_1 = px.imshow(corr_matrix_1, text_auto=True, aspect="auto")
+        st.plotly_chart(fig_1)
     
     with tab6:
         st.header("Word Map")
@@ -283,6 +297,7 @@ elif page == "Temporal Analysis":
 
     # Crashes by Hour
     st.subheader("Crashes by Hour")
+    st.markdown("For the hour, we can see lots of count in 0 because the hour was missing in the data so it was put in as 0 instead.")
     filtered_df['hour'] = pd.to_numeric(filtered_df['hour'], errors='coerce')
     crashes_by_hour = filtered_df['hour'].value_counts().sort_index()
     fig_hours = px.bar( x=crashes_by_hour.index, y=crashes_by_hour.values, title='Number of Crashes by Hour', labels={'x': 'Hour of the Day', 'y': 'Count of Crashes'})
@@ -295,6 +310,7 @@ elif page == "Operator Analysis":
 
     # Top 10 Operators
     st.subheader("Top 10 Operators with Most Crashes")
+    st.markdown("I know this is a repeat. This was supposed to be a placeholder for something else I was trying. This is included in the Exploratory Data Analysis as well.")
     top_operators = filtered_df['operator'].value_counts().nlargest(10)
     fig_operators = px.bar(top_operators, x=top_operators.index, y=top_operators.values, title='Top 10 Operators with Most Crashes', labels={'x': 'Operators', 'y': 'Count of Crashes'})
     st.plotly_chart(fig_operators)
@@ -304,6 +320,7 @@ elif page == "Aircraft Analysis":
 
     # Aircraft Types
     st.subheader("Most Common Aircraft Types")
+    st.markdown(" As the name suggests, this shows the number of crashes for each aircraft and it was surprising to see the dominating aircraft in type of aircrafts.")
     top_aircraft = filtered_df['type'].value_counts().nlargest(10)
     fig_aircraft = px.pie(values=top_aircraft.values, names=top_aircraft.index, title='Top 10 Aircraft Types Involved in Crashes')
     st.plotly_chart(fig_aircraft)
@@ -371,7 +388,7 @@ elif page == "Location Analysis":
 
 elif page == "Animation Analysis":
      st.title("Animated Analysis of Plane Crashes")
-     st.markdown("This is a fun animated chart to see the number of crashes over the years! Just because Animations are fun!")
+     st.markdown("This is a fun animated chart to see the number of crashes over the years! Just because Animations are fun! This shows a timelapse of crashes over the years.")
      animation_tab1, animation_tab2, animation_tab3 = st.tabs([
         "Cumulative Crashes Over Time", 
         "Yearly Accident Trends", 
@@ -505,7 +522,7 @@ elif page == "Animation Analysis":
 
 elif page == "Aircraft Survival Analysis":
     st.title("Aircraft Type Survival Analysis")
-    
+    st.markdown("This page shows the survival rate of the passengers in case of an accident. ")
     # Calculate survival statistics for each aircraft type
     def get_aircraft_survival_stats(df):
         stats = df.groupby('type').agg({
@@ -678,6 +695,15 @@ elif page == "Aircraft Survival Analysis":
         )
         
         st.write(comparison_text)
+elif page == "Summary":
+    st.title("Findings and Future Outlook")
+    st.markdown("I was searching the internet for a complete dataset. My original idea was to investigate the Bermuda Triangle using flight statistics and Weather data to see if there's any correlation with the incidents that occured in Bermuda Triangle with the weather patterns of that period. ")
+    st.markdown(" But, I have been unable to locate flight data for the Bermuda Triangle. I have scraped data from a few websites for crashes all over the world and the shipwrecks. Till now for the Bermuda region, I have found very few recorded shipwrecks and aircraft crashes. I have found a website for the weather data luckily. But, I am curerntly working on scraping the data and then combining it on my local computer. Then, the next step would be filter it down to the locations of San Juan, Bermuda and Florida.")
+    st.markdown(" So, I merged two different datasets for crash data for the airplanes. Then, did an analysis on the survival rate, common crash locations and analysis based on different aircrafts, how many crashes occured around the world throughout the years. Since I was originally focusing on data from 1950-1980, I gathered crash data before 2000.")
+    st.markdown(" I think after collecting the weather data and doing some analysis, I will need to decide whether I should continue to focus on the Bermuda Triangle. I hoped for better data available. It is hard to find historical data. Lots of data are available in paper format. It will take wayyy too much time if I were to convert the data into a digital format. ")
+    st.markdown(" I tried to salvage as much as I could from the data that I had. I tried merging the marine shipwreck logs but the columns are completely different. I ran a separate analysis on that dataset. I am thinking of including that in my final project and try to collect more data related to shipwrecks and aircraft crashes. ")
+    st.markdown(" The dates are missing for half the dataset as they have the month but I could not find the exact date. I was thinking of imputing the data(the missing dates) and then do the analysis but it did not feel correct to me as it won't correctly show the timeline of the crashes.")
+    st.markdown(" My next steps in terms of project is going to look at the weather data and then check for Bermuda region. If there's any sensible data, I will work with that. Other than that, I am a bit confused on how to proceed further. I am considering if I should continue on the same path or change the project entirely.")
 
 # Display raw data option (available on all pages)
 if st.checkbox("Show Raw Data"):
